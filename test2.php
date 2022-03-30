90d75152d36389381d38629dc0f36ecba40e666b
tatanox <95684648+tatanox@users.noreply.github.com>
2022-03-30 15:43:46 +0200

    another-test

--- test2.php
+++ test2.php
@@ -27,52 +27,37 @@
 
 
 // Include the database configuration file
+
 include 'dbConfig.php';
 $statusMsg = '';
 
 // File upload path
 $targetDir = "uploads/";
-$fileName = basename($_FILES["file"]["name"]);
+$fileName = basename($_FILES["file"]["name"]).$fileid;
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-$count = $requete->rowCount();
-
+$fileid = "SELECT * FROM images WHERE id = :id "
 
 if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
     // Allow certain file formats
     $allowTypes = array('jpg','png','jpeg','gif','pdf');
     if(in_array($fileType, $allowTypes)){
-                if($count == 1){
-                function generateRandomString($length = 26) {
-                    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-                    $charactersLength = strlen($characters);
-                    $randomString = '';
-                    for ($i = 0; $i < $length; $i++) {
-                        $randomString .= $characters[rand(0, $charactersLength - 1)];
-                    }
-                    return $randomString;
-                }
-
-                            // Echo the random string.
-                            // Optionally, you can give it a desired string length.
-                            echo generateRandomString();
-                        }else{
-                    // Upload file to server
-                    if(move_uploaded_file($_FILES["file" . "generateRandomString"]["tmp_name"], $targetFilePath)){
-                        // Insert image file name into database
-                        $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
-                        if($insert){
-                            $statusMsg = "L'image ".$fileName. " à été upload correctement.";
-                        }else{
-                            $statusMsg = "L'upload de l'image à échoué, merci de réesssayez.";
-                        } 
-                    }else{
-                        $statusMsg = "Malheureusement, il y a eu une erreur lors de l'upload de l'image.";
-                    }
-                }
+        
+        // Upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            // Insert image file name into database
+            $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+            if($insert){
+                $statusMsg = "L'image ".$fileName. " à été upload correctement.";
+            }else{
+                $statusMsg = "L'upload de l'image à échoué, merci de réesssayez.";
+            } 
+        }else{
+            $statusMsg = "Malheureusement, il y a eu une erreur lors de l'upload de l'image.";
+        }
     }else{
         $statusMsg = 'Désolé, seul les formats suivents sont autorisés : JPG, JPEG, PNG, GIF et PDF.';
-      }
+    }
 }else{
     $statusMsg = 'Merci de choisir un fichier à upload.';
 }
@@ -81,8 +66,6 @@ if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
 echo $statusMsg;
 
     
-
-  
 
 
     //ou ça 
--- upload.php
+++ upload.php
@@ -8,14 +8,14 @@ $targetDir = "uploads/";
 $fileName = basename($_FILES["file"]["name"]);
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-$count = $requete->rowCount();
+$count = $fileName->rowCount();
 
 
 if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
     // Allow certain file formats
     $allowTypes = array('jpg','png','jpeg','gif','pdf');
     if(in_array($fileType, $allowTypes)){
-        
+
         // Upload file to server
         if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
             // Insert image file name into database
